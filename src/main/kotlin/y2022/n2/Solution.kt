61651e2b42ffa5a01f23d0f3d3c616d0fb2a64b1
arranzropablo <arranzropablo@gmail.com>
2023-12-02 12:05:56 +0100

    2022 problem 2 part 2

--- src/main/kotlin/y2022/n2/Solution.kt
+++ src/main/kotlin/y2022/n2/Solution.kt
@@ -10,11 +10,15 @@ object Solution {
     val mePaper = "Y"
     val meScissors = "Z"
 
+    val wantedLose = "X"
+    val wantedDraw = "Y"
+    val wantedWin = "Z"
+
     val rockScore = 1
     val paperScore = 2
     val scissorsScore = 3
 
-    val lostScore = 0
+    val loseScore = 0
     val drawScore = 3
     val winScore = 6
 
@@ -32,13 +36,13 @@ object Solution {
                         me == mePaper ->
                             winScore + paperScore
                         me == meScissors ->
-                            lostScore + scissorsScore
+                            loseScore + scissorsScore
                         else -> 0
                     }
                 elfPaper ->
                     when (me) {
                         meRock ->
-                            lostScore + rockScore
+                            loseScore + rockScore
                         mePaper ->
                             drawScore + paperScore
                         meScissors ->
@@ -50,7 +54,7 @@ object Solution {
                         meRock ->
                             winScore + rockScore
                         mePaper ->
-                            lostScore + paperScore
+                            loseScore + paperScore
                         meScissors ->
                             drawScore + scissorsScore
                         else -> 0
@@ -61,8 +65,47 @@ object Solution {
         return result
     }
 
-    fun part2(inputLines: List<String>): Unit {
+    fun part2(inputLines: List<String>): Int {
+        var result = 0
+        inputLines.forEach { line ->
+            val elf = line.split(" ")[0]
+            val wantedResult = line.split(" ")[1]
 
+            result += when (elf) {
+                elfRock ->
+                    when(wantedResult) {
+                        wantedLose ->
+                            loseScore + scissorsScore
+                        wantedDraw ->
+                            drawScore + rockScore
+                        wantedWin ->
+                            winScore + paperScore
+                        else -> 0
+                    }
+                elfPaper ->
+                    when(wantedResult) {
+                        wantedLose ->
+                            loseScore + rockScore
+                        wantedDraw ->
+                            drawScore + paperScore
+                        wantedWin ->
+                            winScore + scissorsScore
+                        else -> 0
+                    }
+                elfScissors ->
+                    when(wantedResult) {
+                        wantedLose ->
+                            loseScore + paperScore
+                        wantedDraw ->
+                            drawScore + scissorsScore
+                        wantedWin ->
+                            winScore + rockScore
+                        else -> 0
+                    }
+                else -> 0
+            }
+        }
+        return result
     }
 
 }
